41dcd89498abc88f5cd6a0a94cecdbc94b5ec984
Auxifruit <auxi.fruit@gmail.com>
2024-06-20 15:36:38 +0200

    Campus selection filter

--- src/components/EventComponent/OngoingEventsComponent.tsx
+++ src/components/EventComponent/OngoingEventsComponent.tsx
@@ -1,61 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next';
-import { ApiResponseEvents, SomeEventsData } from '../../Tools/Interfaces/EventInterface';
-import { IonGrid, IonLabel, IonSpinner, IonTabButton } from '@ionic/react';
+import { SomeEventsData } from '../../Tools/Interfaces/EventInterface';
+import { IonGrid, IonLabel } from '@ionic/react';
 import EventCardComponent from './EventCardComponent';
 
 import '../../theme/Event/EventsComponent.css';
+import useEventData from '../../Tools/EventApiCall';
 
 const FuturEventsComponent: React.FC<{ apiHref: string }> = ({ apiHref }) => {
     // Use to translate the page
     const { t } = useTranslation();
 
-    const [data, setData] = useState<SomeEventsData[]>([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://tekiens.net/api/' + apiHref);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-
-                const result: ApiResponseEvents = await response.json();
-
-                const eventsWithAssociations: SomeEventsData[] = await Promise.all(result.data.map(async (event) => {
-                    const associationResponse = await fetch(`https://tekiens.net/api/assos/${event.asso_id}`);
-                    if (!associationResponse.ok) {
-                        throw new Error(`Failed to fetch association ${event.asso_id}`);
-                    }
-                    const associationResult = await associationResponse.json();
-                    return {
-                        ...event,
-                        associationName: associationResult.data.names[0], // Use the first name (assuming it's primary)
-                        associationColor: associationResult.data.color,
-                    };
-                }));
-
-                setData(eventsWithAssociations.reverse());
-
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, [apiHref]);
-
-    if (loading) {
-        return (
-            // To center the loading circle
-            <IonTabButton disabled>
-                <IonSpinner name='circular' />
-            </IonTabButton>
-        );
-    }
+    const { data, loading } = useEventData(apiHref);
 
     /**
      * Method to return the filtered data for the ongoing events
