5b11e47dd7c4b5dfd1eb453fc2dcab2553559851
Auxifruit <auxi.fruit@gmail.com>
2024-06-25 16:26:34 +0200

    Connexion

--- src/pages/Settings/ConnexionPage.tsx
+++ src/pages/Settings/ConnexionPage.tsx
@@ -6,26 +6,63 @@ const PageConnexion: React.FC = () => {
 
     const [errorText, setErrorText] = useState('');
 
-    const handleLogin = (event: { preventDefault: () => void; currentTarget: any; }) => {
+    const handleLogin = async (event: { preventDefault: () => void; currentTarget: any; }) => {
         event.preventDefault();
 
         const form = event.currentTarget;
         const values = Object.fromEntries(new FormData(form));
 
-        if (values.assoId === '' && values.password === '') {
+        const assoId = values.assoId as string;
+        const password = values.password as string;
+
+        if (assoId === '' && password === '') {
             setErrorText('Missings parameters')
         }
-        else if (values.assoId === '') {
+        else if (assoId === '') {
             setErrorText('Missing associations ID')
         }
-        else if (values.password === '') {
+        else if (password === '') {
             setErrorText('Missing password')
         }
         else {
             setErrorText('')
+            try {
+                console.log(password);
+
+                const session = await ApiSession(assoId, password);
+
+                console.log('Session ID:', session.id);
+            } catch (error) {
+                console.error('Erreur lors de la connexion:', error);
+                setErrorText('Failed to login');
+            }
         }
     }
 
+    const ApiSession = async (assoId: string, hashPassword: string): Promise<any> => {
+        try {
+            const response = await fetch(`https://tekiens.net/api/sessions`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    asso: assoId,
+                    hash: hashPassword,
+                }),
+            });
+
+            if (!response.ok) {
+                const error = await response.json();
+                throw new Error(error.message || 'Failed to create session');
+            }
+
+            return await response.json();
+        } catch (error) {
+            console.error('Error creating session:', error);
+            throw new Error('Failed to create session');
+        }
+    }
 
     return (
         <IonPage>
