b1b7db79b69e8fe88b8e299fc2083db8c1329fe0
Auxifruit <auxi.fruit@gmail.com>
2024-06-25 11:36:31 +0200

    page connexion

--- src/pages/PageConnexion.tsx
+++ src/pages/PageConnexion.tsx
@@ -1,14 +1,20 @@
-import { IonContent, IonInput, IonPage } from '@ionic/react'
+import { IonButton, IonContent, IonInput, IonLabel, IonPage } from '@ionic/react'
 import HeaderTitleBack from '../components/HeaderTitleBack'
 
 const PageConnexion = () => {
     return (
         <IonPage>
             <HeaderTitleBack back='/app/plus'>Connexion</HeaderTitleBack>
-            <IonContent>
-                <IonInput label="Outline input" labelPlacement="floating" fill="outline" placeholder="Enter text"></IonInput>
-                <IonInput label="Solid input" labelPlacement="floating" fill="solid" placeholder="Enter text"></IonInput>
+            <IonContent className='ion-padding'>
+                <div className='center-screen'>
+                    <IonLabel className='title' style={{ 'font-size': '200%', 'margin-bottom': '10%' }}>Connexion</IonLabel>
+                    <IonInput label="Id de l'association" labelPlacement="floating" placeholder="Saisir l'id de l'association" type='text' clearInput={true} fill="outline" className='login-item' />
+                    <IonInput label="Mot de passe" labelPlacement="floating" placeholder="Saisir le mot de passe" type='password' clearInput={true} fill="outline" className='login-item' />
+                    <IonButton className='login-item' style={{ 'width': '100%' }}>Se connecter</IonButton>
+                    <span className='error'>fsd</span>
+                </div>
             </IonContent>
+
         </IonPage>
     )
 }
