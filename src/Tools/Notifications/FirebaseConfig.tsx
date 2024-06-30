2bb86cc01c4ef39cec892f43c831fdce4afe6057
test <raph.sencee@gmail.com>
2024-06-30 21:50:30 +0200

    Push notifications logic (Android)

---
+++ src/Tools/Notifications/FirebaseConfig.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7A0HAmmd-OwI-kIF5jIz3sNzNEiRh5WI",
+    authDomain: 'tekiens-app.firebaseapp.com',
+    projectId: "tekiens-app",
+    storageBucket: "tekiens-app.appspot.com",
+    messagingSenderId: "134009207786",
+    appId: "1:134009207786:android:30f40bef9c95df0fbd5093",
+  };
+  
+const app = initializeApp(firebaseConfig);
