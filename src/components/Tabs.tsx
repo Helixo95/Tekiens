131287bb67373d7a5bfcebf3e0cf102e632e0eb7
Auxifruit <auxi.fruit@gmail.com>
2024-07-09 10:58:12 +0200

    Badge if new events

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,4 +1,4 @@
-import { IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react'
+import { IonBadge, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react'
 import { Redirect, Route } from 'react-router'
 import { homeOutline, starOutline, calendarOutline, readerOutline, settingsOutline } from 'ionicons/icons';
 import Home from "../pages/HomePage";
@@ -7,10 +7,44 @@ import Favorite from '../pages/FavoritePage';
 import Settings from '../pages/Settings/SettingsPage';
 import Associations from '../pages/Association/Associations';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+import Api from '../Tools/Api';
+import { EventData } from '../Tools/Interfaces/EventAndAssoInterface';
 
 const Tabs: React.FC = () => {
     // Use to translte the page
     const { t } = useTranslation();
+
+    const [newEventCount, setNewEventCount] = useState<number | null>(null);
+
+    const fetchNewEvents = async () => {
+        const lastCheckedDate = localStorage.getItem('lastCheckedDate');
+        const lastSavedDate = lastCheckedDate ? new Date(lastCheckedDate) : new Date();
+        const events = await Api.event.get();
+        let newEvents = 0;
+
+        events.map((event: EventData) => {
+            const eventCreationDate = new Date(event.createDate);
+
+            eventCreationDate.setHours(eventCreationDate.getHours() + 2);
+
+            if (eventCreationDate > lastSavedDate) {
+                newEvents++;
+            }
+        })
+
+        if (newEvents > 0) {
+            setNewEventCount(newEvents);
+        } else {
+            setNewEventCount(null);
+        }
+
+        localStorage.setItem('lastCheckedDate', new Date().toISOString());
+    };
+
+    useEffect(() => {
+        fetchNewEvents();
+    }, []);
 
     return (
         <IonTabs>
@@ -27,27 +61,28 @@ const Tabs: React.FC = () => {
             </IonRouterOutlet>
 
             <IonTabBar slot='bottom'>
-                <IonTabButton tab='home' href='/app/home'>
+                <IonTabButton tab='home' onClick={() => fetchNewEvents()} href='/app/home'>
                     <IonIcon icon={homeOutline} />
                     <IonLabel>{t('home.title')}</IonLabel>
                 </IonTabButton>
 
-                <IonTabButton tab='associations' href='/app/associations'>
+                <IonTabButton tab='associations' onClick={() => fetchNewEvents()} href='/app/associations'>
                     <IonIcon icon={readerOutline} />
                     <IonLabel>{t('associations.tab-title')}</IonLabel>
                 </IonTabButton>
 
-                <IonTabButton tab='events' href='/app/events'>
+                <IonTabButton tab='events' onClick={() => setNewEventCount(null)} href='/app/events'>
                     <IonIcon icon={calendarOutline} />
+                    <IonBadge>{newEventCount}</IonBadge>
                     <IonLabel>{t('events.tab-title')}</IonLabel>
                 </IonTabButton>
 
-                <IonTabButton tab='favorite' href='/app/favorite'>
+                <IonTabButton tab='favorite' onClick={() => fetchNewEvents()} href='/app/favorite'>
                     <IonIcon icon={starOutline} />
                     <IonLabel>{t('favorite.title')}</IonLabel>
                 </IonTabButton>
 
-                <IonTabButton tab='settings' href='/app/settings'>
+                <IonTabButton tab='settings' onClick={() => fetchNewEvents()} href='/app/settings'>
                     <IonIcon icon={settingsOutline} />
                     <IonLabel>{t('settings.title')}</IonLabel>
                 </IonTabButton>
