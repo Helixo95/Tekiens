eea6864f336fa10d1e4c579dceeb19fc5dd2c83e
Auxifruit <auxi.fruit@gmail.com>
2024-07-22 10:58:54 +0200

    Nouveau css pour les images des cartes des events

--- src/components/EventComponent/EventCardComponent.tsx
+++ src/components/EventComponent/EventCardComponent.tsx
@@ -1,7 +1,8 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonLabel, IonRow } from '@ionic/react'
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonImg, IonLabel, IonRow } from '@ionic/react'
 import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { AssosData, EventData } from '../../Tools/Interfaces/EventAndAssoInterface';
+import '../../theme/Event/EventCard.css'
 
 
 const EventCardComponent: React.FC<{ event: EventData, asso: AssosData | undefined }> = ({ event, asso }) => {
@@ -10,7 +11,7 @@ const EventCardComponent: React.FC<{ event: EventData, asso: AssosData | undefin
 
     return (
         <IonCard className='event-card' button={true} href={'/event/' + event.id} >
-            <img src={event.poster ? `${event.poster}?${Date.now()}` : ""} />
+            <IonImg className='event-image' src={event.poster ? `${event.poster}?${Date.now()}` : ""} style={{ backgroundColor: asso?.color }} />
             <IonCardHeader>
                 <IonCardTitle>{event.title}</IonCardTitle>
             </IonCardHeader>
