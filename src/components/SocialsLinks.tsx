99252094c47ebdc7566412674e1ee73e418c3165
Guillaume B. <105287640+Auxifruit@users.noreply.github.com>
2025-02-09 17:53:52 +0100

    Correction bug asso social

--- src/components/SocialsLinks.tsx
+++ src/components/SocialsLinks.tsx
@@ -1,21 +1,21 @@
 import { IonButton, IonIcon, IonInput, IonItem, IonList, IonSelect, IonSelectOption } from '@ionic/react';
-import { add, call, callOutline, closeOutline } from 'ionicons/icons';
-import React, { useEffect, useState } from 'react';
-import { SocialType, SocialsDisplay, SocialsURL } from '../Tools/Interfaces/EventAndAssoInterface';
+import { add, closeOutline } from 'ionicons/icons';
+import React from 'react';
+import { SocialsData, SocialType } from '../Tools/Interfaces/EventAndAssoInterface';
 
-const SocialLinks: React.FC<{ socials: string[][], callback: Function }> = ({ socials, callback }) => {
+const SocialLinks: React.FC<{ socials: SocialsData[], callback: Function }> = ({ socials, callback }) => {
     // Change the type of social of the current socials index
     function handleSocialChange(e: CustomEvent, index: any) {
         // Copy the values of the array, modify them and update 'socials' thanks to its callback setter
         const copy = [...socials];
-        copy[index][0] = e.detail.value;
+        copy[index] = { ...copy[index], id: e.detail.value };
 
         callback(copy);
     }
 
     function handleSocialIDChange(e: CustomEvent, index: any) {
         const copy = [...socials];
-        copy[index][1] = e.detail.value;
+        copy[index] = { ...copy[index], value: e.detail.value };
 
         callback(copy);
     }
@@ -23,7 +23,7 @@ const SocialLinks: React.FC<{ socials: string[][], callback: Function }> = ({ so
     // Add a single entry to the social array
     function handleAdding() {
         const socialsCopy = [...socials];
-        socialsCopy.push([]);
+        socialsCopy.push({ id: "", display: "", link: "", value: "" });
 
         callback(socialsCopy);
     }
@@ -39,20 +39,22 @@ const SocialLinks: React.FC<{ socials: string[][], callback: Function }> = ({ so
             <IonButton slot='end' onClick={handleAdding} expand="block"><IonIcon icon={add} /></IonButton>
             <IonList>
                 {
-                    socials.map((val, index) => (
+                    socials.map((social, index) => (
                         <IonItem key={index} lines={index === socials.length - 1 ? 'none' : undefined}>
                             <IonIcon slot='end' icon={closeOutline} onClick={() => handleDelelition(index)} />
 
                             <IonInput
                                 type="text"
                                 onIonInput={(e) => handleSocialIDChange(e, index)}
-                                placeholder={val[1]}
+                                placeholder="Id"
+                                value={social.value}
                             />
 
                             <IonSelect
                                 interface='popover'
                                 onIonChange={(e) => handleSocialChange(e, index)}
-                                placeholder={val[0]}
+                                placeholder="Select"
+                                value={social.id}
                             >
                                 {Object.values(SocialType).map((SocialVal, index) => (
                                     <IonSelectOption key={index} value={SocialVal}>{SocialVal}</IonSelectOption>
